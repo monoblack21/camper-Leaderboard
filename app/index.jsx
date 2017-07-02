192463abcaccd23942ebbaee237083d71a21feb7
ayo aloko <kalming@yahoo.com>
2017-07-02 22:19:47 +0300

    made Content and Leaderboard component and added axios lib

--- app/index.jsx
+++ app/index.jsx
@@ -2,6 +2,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import style from './style/main.scss';
 
+class Content extends React.Component{
+    construnctor(props){
+        super(props);
+    }
+    render(){
+        return (
+            <tbody>
+               
+            </tbody>
+        );
+    }
+}
 
-ReactDOM.render(<h1>swag</h1>, document.getElementById('root')
+class LeaderBoard extends React.Component{
+    constructor(props){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div>
+                <table>
+                    <caption>LeaderBoard</caption>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Camper name</th>
+                            <th>Points in past 30 days</th>
+                            <th>All time points</th>
+                        </tr>
+                    </thead>
+                </table>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <LeaderBoard />, document.getElementById('root')
 );
