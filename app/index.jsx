44eac0932b84a81c0a31d049b946ea8e5180bec0
ayo aloko <kalming@yahoo.com>
2017-07-03 00:27:35 +0300

    react is functional!!

--- app/index.jsx
+++ app/index.jsx
@@ -2,22 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import style from './style/main.scss';
 
-class Content extends React.Component{
-    construnctor(props){
-        super(props);
-    }
-    render(){
+const axios = require("axios");
+
+function Tbod (props){
+    let body = props.data.map((obj, iter) => {
         return (
-            <tbody>
-               
-            </tbody>
+            <tr key = {obj.img}>
+                <td>{++iter}</td>
+                <td><img src={obj.img} /> <a href={"https://freecodecamp.com/" + obj.username}>{obj.username}</a></td>
+                <td>{obj.recent}</td>
+                <td>{obj.alltime}</td>
+            </tr>
         );
-    }
+    });
+    return (
+        <tbody>
+            {body}
+        </tbody>
+    );
 }
 
 class LeaderBoard extends React.Component{
     constructor(props){
         super(props);
+        this.handleClick = this.handleClick.bind(this);
+        this.state = {
+            isDays: true,
+            days: [],
+            allTime:[]
+        }
+    }
+
+    getData(url, which){
+        let thus = this;
+        axios.get(url).then((response) => {
+            thus.setState({
+                [which] : response.data
+            });
+        }).catch((error) => {
+                console.log(error);
+            });
+    }
+
+    componentDidMount(){
+        this.getData("https://fcctop100.herokuapp.com/api/fccusers/top/recent", "days");
+        this.getData("https://fcctop100.herokuapp.com/api/fccusers/top/alltime", "allTime");
+    }
+
+    handleClick(which){
+        let bool = which == "days" ? true : false;
+        this.setState({
+            isDays: bool
+        });
     }
 
     render(){
@@ -29,10 +65,11 @@ class LeaderBoard extends React.Component{
                         <tr>
                             <th>#</th>
                             <th>Camper name</th>
-                            <th>Points in past 30 days</th>
-                            <th>All time points</th>
+                            <th onClick = {() => this.handleClick("days")}>Points in past 30 days</th>
+                            <th onClick = {() => this.handleClick("allTime")}>All time points</th>
                         </tr>
                     </thead>
+                    <Tbod data = {this.state.isDays ? this.state.days : this.state.allTime}/>
                 </table>
             </div>
         );
